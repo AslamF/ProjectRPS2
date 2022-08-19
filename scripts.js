4defb40c0eb5e0ffd2761d7d5a4c3f4ab06ec9ce
Fraz Aslam <faslam7646@gmail.com>
2022-08-18 23:13:07 -0400

    this was a lenghty commit, got everything up and running well. what troubled me was using console.log effectively.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,67 @@
+let playerCount = 0;
+let computerCount = 0;
 
-function getCompterChoice(){
-    let choices = [
-        "Rock",
-        "Paper",
-        "Scissors"
-    ];
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
+// PART 1 - FUNCTION THAT PRODUCES A RANDOM CHOICE FOR THE COMPUTER 
+
+function getComputerChoice(){
+    let choices = ["Rock", "Paper", "Scissors"];
+    const generate = choices[Math.floor(Math.random() * choices.length)];
+    return generate;
 }
-console.log(getCompterChoice());
+
+// PART 2 - A SINGLE GAME OF ROCK/PAPER/SCISSOR, YOU DONT NEED TO ADD PLAYER INPUT AT THIS POINT
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return ("its a draw! no one wins!");
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors"){
+        computerCount++;
+        return ("You Lose! Rock beats scissors");
+        
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Paper"){
+        playerCount++;
+        return ("You Win! Paper beats rock!");
+        
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Rock"){
+        computerCount++;
+        return ("You Lose! Paper beats Rock");
+        
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Scissors"){
+        playerCount++;
+        return ("You Win! Scissors beats Paper!");
+        
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Rock"){
+        playerCount++;
+        return ("You Win! Rock beats scissors");
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper"){
+        computerCount++;
+        return ("You Lose! Scissors beats paper");
+    }
+}
+
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Rock,Paper, or Scissors? ");
+        let newText = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        playerSelection = newText;
+        const computerSelection = getComputerChoice();
+        (playRound(playerSelection, computerSelection));
+    }
+    if (playerCount > computerCount){
+        return ("You beat comp!");
+    }
+    else if (playerCount < computerCount){
+        return ("you lost");
+    }
+    else{
+        return ("its a tie!?!?!?!!?");
+    }
+}
+console.log(game());
