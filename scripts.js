ed85367a90046fa27550437657afb1ab14da3e8d
Fraz Aslam <faslam7646@gmail.com>
2022-09-06 17:24:26 -0400

    got everthhing in working order, UI needs to be updated

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@ function getComputerChoice(){
     const generate = choices[Math.floor(Math.random() * choices.length)];
     return generate;
 };
-let computerSelection
-let playerSelection 
+
+
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0; 
 //const computerSelection = getComputerChoice();
 
 
@@ -15,31 +19,44 @@ let playerSelection
         return ("its a draw! no one wins!");
     }
     else if (computerSelection === "Rock" && playerSelection === "Scissors"){
-        
+        computerScore++;
+
+        cscore.textContent = "Computer Score: " + computerScore;
         return ("You Lose! Rock beats scissors");
+        
         
     }
     else if (computerSelection === "Rock" && playerSelection === "Paper"){
+        playerScore++;
         
+        pscore.textContent =  "Player Score: " + playerScore;
         return ("You Win! Paper beats rock!");
         
     }
     else if (computerSelection === "Paper" && playerSelection === "Rock"){
-        
+        computerScore++;
+
+        cscore.textContent = "Computer Score: " + computerScore;
         return ("You Lose! Paper beats Rock");
         
     }
     else if (computerSelection === "Paper" && playerSelection === "Scissors"){
+        playerScore++;
         
+        pscore.textContent =  "Player Score: " + playerScore;
         return ("You Win! Scissors beats Paper!");
         
     }
     else if (computerSelection === "Scissors" && playerSelection === "Rock"){
-        
+        playerScore++;
+  
+        pscore.textContent =  "Player Score: " + playerScore;
         return ("You Win! Rock beats scissors");
     }
     else if (computerSelection === "Scissors" && playerSelection === "Paper"){
-        
+        computerScore++;
+
+        cscore.textContent = "Computer Score: " + computerScore;
         return ("You Lose! Scissors beats paper");
     }
     
@@ -50,30 +67,66 @@ let playerSelection
 const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorButton = document.querySelector(".scissor");
+const pscore = document.querySelector(".playerScore"); 
+const cscore = document.querySelector(".computerScore");
+const container = document.querySelector(".results");
+const runningScore = document.querySelector(".running-score");
+const results = document.querySelector(".results");
+const buttons = document.querySelectorAll("button");
 
-
-
-
-
-
-
+function disableButtons(){
+for (let i =0; i < buttons.length; i++){
+    buttons[i].disabled = true;;
+}};
 
 rockButton.addEventListener("click",() => {
     const computerSelection = getComputerChoice();
     playerSelection = "Rock";
     playRound();
-    console.log(playRound(playerSelection, computerSelection));
+    results.textContent = playRound(playerSelection, computerSelection);
+    gameWinner();
+    /*const score = document.querySelector(".playerScore");
+    score.textContent = playerScore; */
+    
+    
+    
 } )
 
 paperButton.addEventListener("click",() => {
     const computerSelection = getComputerChoice();
     playerSelection = "Paper";
     playRound();
-    console.log(playRound(playerSelection, computerSelection));
+    results.textContent = playRound(playerSelection, computerSelection);
+    gameWinner();
+    
+
 } )
 scissorButton.addEventListener("click",() => {
     const computerSelection = getComputerChoice();
     playerSelection = "Scissors";
     playRound();
-    console.log(playRound(playerSelection, computerSelection));
+    results.textContent = playRound(playerSelection, computerSelection);
+    gameWinner();
+
 } )
+
+function gameWinner(){
+if (playerScore === 5){
+    
+    const winner = document.createElement("div");
+    winner.textContent = "CONGRATS YOU BEAT THE AI!";
+    runningScore.appendChild(winner);
+    disableButtons();
+        
+}
+else if (computerScore ===5){
+    const winner = document.createElement("div");
+    winner.textContent = "THE AI HAS WON, YOU LOST";
+    runningScore.appendChild(winner);
+    disableButtons();
+    
+}
+};
+
+
+
